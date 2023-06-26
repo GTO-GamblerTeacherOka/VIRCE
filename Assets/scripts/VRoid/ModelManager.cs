5225898b41d494d71396f1b4a9aa66f5e37235b6
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-06-26 22:10:11 +0900

    update: add Collider and rigit body at loaded VRM when model load

--- Assets/scripts/VRoid/ModelManager.cs
+++ Assets/scripts/VRoid/ModelManager.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
+using Pixiv.VroidSdk;
 using UnityEngine;
 
-public class ModelManager : MonoBehaviour
+namespace VRoid
 {
-    // Start is called before the first frame update
-    void Start()
+    public class ModelManager : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Start()
+        {
+            Auth.Api.GetAccountCharacterModels(10, models =>
+            {
+                ModelLoader.LoadVrm(models[0], vrm =>
+                {
+                    vrm.transform.localPosition = new Vector3(0, 0, 0);
+                    vrm.transform.localRotation = Quaternion.Euler(0, 180, 0);
+                    vrm.transform.localScale = Vector3.one * 5;
+                    var colliderComponent = vrm.gameObject.AddComponent<CapsuleCollider>();
+                    var height = vrm.GetComponent<Animator>().GetBoneTransform(HumanBodyBones.Head).position.y - vrm.GetComponent<Animator>().GetBoneTransform(HumanBodyBones.Hips).position.y;
+                    colliderComponent.height = height;
+                    colliderComponent.radius = height / 8;
+                    colliderComponent.center = new Vector3(0, height / 2, 0);
+                    var rigitBodyComponent = vrm.gameObject.AddComponent<Rigidbody>();
+                    rigitBodyComponent.useGravity = true;
+                    rigitBodyComponent.mass = 60;
+                    rigitBodyComponent.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+                }, progress =>
+                {
+                    Debug.Log(progress);
+                }, _ => {});
+            }, _ =>{});
+        }
     }
 }
