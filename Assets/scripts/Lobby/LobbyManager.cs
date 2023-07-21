f58d6dd9b93d26f0809effdb9f64dace290b6848
maesuke269 <maesuke1221@gmail.com>
2023-07-21 10:36:50 +0900

    Update positon vector

--- Assets/scripts/Lobby/LobbyManager.cs
+++ Assets/scripts/Lobby/LobbyManager.cs
@@ -10,6 +10,21 @@ namespace Lobby
     public class LobbyManager : MonoBehaviour
     {
         public static Dictionary<string, GameObject> userObjects;
-        
+
+        private void Update()
+        {
+            //TODO:get data from DataBuf
+            var body = new byte[24];
+            var x = body[0..4];
+            var y = body[4..8];
+            var z = body[8..12];
+
+            var vec = new Vector3(
+                BitConverter.ToSingle(x), 
+                BitConverter.ToSingle(y), 
+                BitConverter.ToSingle(z));
+            
+            
+        }
     }
 }
