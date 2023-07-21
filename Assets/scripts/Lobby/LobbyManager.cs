5a9ac371fdb20532d512b018fe2f83d7433c3d89
maesuke269 <maesuke1221@gmail.com>
2023-07-21 11:15:01 +0900

    add obj.transform

--- Assets/scripts/Lobby/LobbyManager.cs
+++ Assets/scripts/Lobby/LobbyManager.cs
@@ -9,29 +9,38 @@ namespace Lobby
     /// </summary>
     public class LobbyManager : MonoBehaviour
     {
-        public static Dictionary<string, GameObject> userObjects;
+        private static Dictionary<string, GameObject> _userObjects;
 
-        private void Update()
+        public void Update()
         {
-            //TODO:get data from DataBuf
-            var body = new byte[24];
-            var x = body[0..4];
-            var y = body[4..8];
-            var z = body[8..12];
+            foreach (var keyValuePair in _userObjects)
+            {
+                var key = keyValuePair.Key;
+                var obj = keyValuePair.Value;
+                
+                //TODO:get data from DataBuf
+                var body = new byte[24];
+                var x = body[0..4];
+                var y = body[4..8];
+                var z = body[8..12];
 
-            var vec = new Vector3(
-                BitConverter.ToSingle(x), 
-                BitConverter.ToSingle(y), 
-                BitConverter.ToSingle(z));
+                var vec = new Vector3(
+                    BitConverter.ToSingle(x), 
+                    BitConverter.ToSingle(y), 
+                    BitConverter.ToSingle(z));
 
-            var anglex = body[12..16];
-            var angley = body[16..20];
-            var anglez = body[20..24];
+                var anglex = body[12..16];
+                var angley = body[16..20];
+                var anglez = body[20..24];
 
-            var anglevec = new Vector3(
-                BitConverter.ToSingle(anglex),
-                BitConverter.ToSingle(angley),
-                BitConverter.ToSingle(anglez));
+                var anglevec = new Vector3(
+                    BitConverter.ToSingle(anglex),
+                    BitConverter.ToSingle(angley),
+                    BitConverter.ToSingle(anglez));
+                
+                obj.transform.position = vec;
+                obj.transform.eulerAngles = anglevec;
+            }
         }
     }
 }
