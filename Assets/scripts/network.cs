2a49903f343ce9a6428cc9eb41cee1bcb2066701
wagomuwahaha <mo20041020@gmail.com>
2023-07-28 11:39:47 +0900

    make socket

--- Assets/scripts/network.cs
+++ Assets/scripts/network.cs
@@ -19,23 +19,21 @@ public class UDPreciever : MonoBehaviour
 public void ListenMessage()
 {
     var local = new IPEndPoint(IPAddress.Any, 5000);
-    var remote = new IPEndPoint(IPAddress.Any, 5000) as EndPoint;
-    
-    var socket = new Socket(SocketType.Dgram, ProtocolType.Udp);
-    socket.Bind(local);
-    
-    var e = new SocketAsyncEventArgs();
-    e.RemoteEndPoint = remote;
-    e.SetBuffer(0, 1024);
-    e.Completed += this.OnReceiveCompleted;
-    
-    socket.ReceiveFromAsync(e);
+    var remote = new IPEndPoint(IPAddress.Any, 5000);
+    var client = new UdpClient(local);
+ 
+    while (true)
+    {
+        var result = await client.ReceiveAsync();
+        
+        var data = Encoding.UTF8.GetString(result.Buffer);
+        
+        this.OnRecieve(data);
+    }
 }
  
-private void OnReceiveCompleted(object sender, SocketAsyncEventArgs e)
+private void OnRecieve(string data)
 {
-    var data = Encoding.UTF8.GetString(e.Buffer);
-    
     public static void WriteBinaryToFile(string path, byte[] data)
     {
         var dir = Path.GetDirectoryName(path);
@@ -50,26 +48,16 @@ private void OnReceiveCompleted(object sender, SocketAsyncEventArgs e)
             sw.Write(data);
         }
     }
-    
-    this.ListenMessage();
 }
-public void SendMessage()
+
+public async void SendMessage()
 {
-    var local = new IPEndPoint(
-        IPAddress.Parse(0),
-        5000);
-    
     var remote = new IPEndPoint(
         IPAddress.Parse("127.0.0.1"),
         5000);
-    
-    var e = new SocketAsyncEventArgs();
-    e.RemoteEndPoint = remote;
-    e.SetBuffer(message, 0, message.Length);
-    
-    var socket = new Socket(SocketType.Dgram, ProtocolType.Udp);
-    socket.Bind(local);
-    socket.SendToAsync(e);
-    socket.Shutdown(SocketShutdown.Both);
-    socket.Close();
+
+    var client = new UdpClient(5000);
+    client.Connect(remote);
+    await client.SendAsync(message, message.Length);
+    client.Close();
 }
