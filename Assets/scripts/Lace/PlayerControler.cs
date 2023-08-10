9250e1803d0137c1bf6a18d2def52978a388b5cb
maesuke269 <maesuke1221@gmail.com>
2023-08-10 23:28:50 +0900

    update:add Inject InputProvider

--- Assets/scripts/Lace/PlayerControler.cs
+++ Assets/scripts/Lace/PlayerControler.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using IO;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using Zenject;
 
 public class PlayerControler : MonoBehaviour
 {
+    [Inject] 
+    private IMoveProvider _moveProvider;
+    [Inject]
+    private IViewPointProvider _viewPointProvider;
+    
     // Start is called before the first frame update
     void Start()
     {
