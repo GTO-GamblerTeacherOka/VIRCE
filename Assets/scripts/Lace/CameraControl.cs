a6cf30f0746d1bbdfbea8586c94c61135f45dc34
gakkey <gakkey610@gmail.com>
2023-08-14 16:48:58 +0900

    Make Start() and partially completed

--- Assets/scripts/Lace/CameraControl.cs
+++ Assets/scripts/Lace/CameraControl.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
-using Zenject;
-using IO;
 
 namespace Lace
 {
     public class CameraControl : MonoBehaviour
     {
-        [Inject]
-        private IViewPointProvider _viewPointProvider;
+        [SerializeField] private Camera vcam;
+
+        private void Start()
+        {
+            vcam.transform.SetParent();
+            vcam.transform.localPosition = new Vector3(0, 1f, -2);
+            vcam.transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
     }
 }
 
