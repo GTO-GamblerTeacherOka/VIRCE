61bdbc132634e000d6eeab0dfc2f7b1b5209b0ee
gakkey <gakkey610@gmail.com>
2023-08-15 17:39:51 +0900

    Implement moveSpeed

--- Assets/scripts/Lace/PlayerControl.cs
+++ Assets/scripts/Lace/PlayerControl.cs
@@ -5,7 +5,9 @@ namespace Lace
 {
    public class PlayerControl : MonoBehaviour
    {
-       
+       [SerializeField]
+       private float moveSpeed = 1.0f;
+
        void Update()
        {
            
