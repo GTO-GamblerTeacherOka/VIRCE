ef9f40f7db8e0731a5121278980e0f418b780367
maesuke269 <maesuke1221@gmail.com>
2023-08-26 23:22:29 +0900

    hotfix:put in namespace Lace

--- Assets/scripts/Lace/PlayerControler.cs
+++ Assets/scripts/Lace/PlayerControler.cs
@@ -5,28 +5,32 @@ using Util;
 using Zenject;
 using Vector3 = UnityEngine.Vector3;
 
-public class PlayerControler : MonoBehaviour
+namespace Lace
 {
-    [Inject] 
-    private IMoveProvider _moveProvider;
-    [Inject]
-    private IViewPointProvider _viewPointProvider;
-
-    [SerializeField] 
-    private float moveSpeed = 1.0f;
-
-    private Vector3 _move;
-    
-    private void Update()
+    /// <summary>
+    /// Class for controlling player in Lace
+    /// </summary>
+    public class PlayerControler : MonoBehaviour
     {
-        var moveComplex = _moveProvider.GetMove();
-        var rotationComplex = new Complex(transform.eulerAngles, true);
+        [Inject] private IMoveProvider _moveProvider;
+        [Inject] private IViewPointProvider _viewPointProvider;
 
-        transform.position += rotationComplex.Conjugate.Normalize.ToVector3 * moveSpeed;
-        
-        //todo: rotate player the Y axis
-        var horizontalRotation = (moveComplex * rotationComplex.Conjugate).Normalize.ToVector3;
-        transform.RotateAround(transform.position, Vector3.up, (horizontalRotation * (moveSpeed * Time.deltaTime)).magnitude);
-        //todo: rotate camera the X axis
+        [SerializeField] private float moveSpeed = 1.0f;
+
+        private Vector3 _move;
+
+        private void Update()
+        {
+            var moveComplex = _moveProvider.GetMove();
+            var rotationComplex = new Complex(transform.eulerAngles, true);
+
+            transform.position += rotationComplex.Conjugate.Normalize.ToVector3 * moveSpeed;
+
+            //todo: rotate player the Y axis
+            var horizontalRotation = (moveComplex * rotationComplex.Conjugate).Normalize.ToVector3;
+            transform.RotateAround(transform.position, Vector3.up,
+                (horizontalRotation * (moveSpeed * Time.deltaTime)).magnitude);
+            //todo: rotate player the X axis
+        }
     }
-}
+}    
