0fc4a6e0bf58480fe1db3584ae879d8745b13b8a
maesuke269 <maesuke1221@gmail.com>
2023-09-03 17:18:52 +0900

    update:add Send process in synchronous processing

--- Assets/scripts/Lace/LaceManager.cs
+++ Assets/scripts/Lace/LaceManager.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Linq;
+using Cysharp.Threading.Tasks;
 
 namespace Lace
 {
@@ -10,6 +13,35 @@ namespace Lace
     {
         private static Dictionary<string, GameObject> _userObjects;
         private static GameObject _currentUserGameObject;
+
+        private void Send()
+        {
+            var sendVec = new float[]
+            {
+                _currentUserGameObject.transform.position.x,
+                _currentUserGameObject.transform.position.y,
+                _currentUserGameObject.transform.position.z
+            };
+            var sendAngleVec = new float[]
+            {
+                _currentUserGameObject.transform.eulerAngles.x,
+                _currentUserGameObject.transform.eulerAngles.y,
+                _currentUserGameObject.transform.eulerAngles.z
+            };
+            var vf = sendVec.Concat(sendAngleVec).ToArray();
+            var body = new byte[24];
+            for (var i = 0; i < vf.Length; i++)
+            {
+                var b = BitConverter.GetBytes(vf[i]);
+                Array.Copy(b, 0, body, i * 4, 4);
+            }
+            
+            //todo:send data with header
+            UniTask.Run(() =>
+            {
+
+            });
+        }
     }
 }
 
