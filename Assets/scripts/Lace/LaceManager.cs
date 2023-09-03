7cc72e2912254f604b1053949ec6935fefb5a85b
maesuke269 <maesuke1221@gmail.com>
2023-09-03 16:53:06 +0900

    update:add _userObjects for use in synchronous processing

--- Assets/scripts/Lace/LaceManager.cs
+++ Assets/scripts/Lace/LaceManager.cs
@@ -1,10 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Lace
 {
+    /// <summary>
+    /// Class for managing lace
+    /// </summary>
     public class LaceManager : MonoBehaviour
     {
-        
+        private static Dictionary<string, GameObject> _userObjects;
     }
 }
 
