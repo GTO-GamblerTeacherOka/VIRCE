12bf8131ec7312c45ef0067b5932c57a6c4c9649
gakkey <gakkey610@gmail.com>
2023-09-20 17:29:45 +0900

    update:Add string of now time in CreateHash

--- Assets/scripts/Protocol/PacketCreator.cs
+++ Assets/scripts/Protocol/PacketCreator.cs
@@ -39,12 +39,7 @@ namespace Protocol
         
         private static uint CreateHash(string str)
         {
-            Message[] msgs = _chatManager.GetChatMessages();
-            foreach (var msg in msgs)
-            {
-                var time = msg.Time;
-                str += time;
-            }
+            str += DateTime.Now;
 
             const int mod = 65521;
             uint a = 1, b = 0;
