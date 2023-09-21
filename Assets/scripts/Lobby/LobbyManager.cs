284c84f5cb8c773adfcbf7d505f82e4fcb369c7a
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-09-21 20:53:29 +0900

    update: add logic to request math to server

--- Assets/scripts/Lobby/LobbyManager.cs
+++ Assets/scripts/Lobby/LobbyManager.cs
@@ -2,27 +2,74 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Cysharp.Threading.Tasks;
+using Networking;
+using Protocol;
+using Settings;
 using UnityEngine;
 
 namespace Lobby
 {
     /// <summary>
-    /// Class for managing lobby
+    ///     Class for managing lobby
     /// </summary>
     public class LobbyManager : MonoBehaviour
     {
         private static Dictionary<string, GameObject> _userObjects;
         private static GameObject _currentUserGameObject;
 
+        private void Start()
+        {
+            _userObjects = new Dictionary<string, GameObject>();
+            var data = PacketCreator.EntryPacket(PacketCreator.EntryType.Lobby, GameSetting.ModelPublishId);
+            Socket.Instance.Send(data);
+        }
+
+        private void Update()
+        {
+            foreach (var keyValuePair in _userObjects)
+            {
+                var key = keyValuePair.Key;
+                var obj = keyValuePair.Value;
+
+                //TODO:get data from DataBuf
+                var body = new byte[24];
+
+                var x = body[..4];
+                var y = body[4..8];
+                var z = body[8..12];
+                var vec = new Vector3(
+                    BitConverter.ToSingle(x),
+                    BitConverter.ToSingle(y),
+                    BitConverter.ToSingle(z));
+
+                var angleX = body[12..16];
+                var angleY = body[16..20];
+                var angleZ = body[20..24];
+
+                var angleVec = new Vector3(
+                    BitConverter.ToSingle(angleX),
+                    BitConverter.ToSingle(angleY),
+                    BitConverter.ToSingle(angleZ));
+
+                obj.transform.position = vec;
+                obj.transform.eulerAngles = angleVec;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            Send();
+        }
+
         private void Send()
         {
-            var sendVec = new float[]
+            var sendVec = new[]
             {
                 _currentUserGameObject.transform.position.x,
                 _currentUserGameObject.transform.position.y,
                 _currentUserGameObject.transform.position.z
             };
-            var sendAngleVec = new float[]
+            var sendAngleVec = new[]
             {
                 _currentUserGameObject.transform.eulerAngles.x,
                 _currentUserGameObject.transform.eulerAngles.y,
@@ -35,49 +82,9 @@ namespace Lobby
                 var b = BitConverter.GetBytes(vf[i]);
                 Array.Copy(b, 0, body, i * 4, 4);
             }
+
             //todo:send data with header
-            UniTask.Run(() =>
-            {
-
-            });
-        }
-
-        private void Update()
-        {
-            foreach (var keyValuePair in _userObjects)
-            {
-                var key = keyValuePair.Key;
-                var obj = keyValuePair.Value;
-                
-                //TODO:get data from DataBuf
-                var body = new byte[24];
-                
-                var x = body[0..4];
-                var y = body[4..8];
-                var z = body[8..12];
-                var vec = new Vector3(
-                    BitConverter.ToSingle(x), 
-                    BitConverter.ToSingle(y), 
-                    BitConverter.ToSingle(z));
-
-                var angleX = body[12..16];
-                var angleY = body[16..20];
-                var angleZ = body[20..24];
-
-                var angleVec = new Vector3(
-                    BitConverter.ToSingle(angleX),
-                    BitConverter.ToSingle(angleY),
-                    BitConverter.ToSingle(angleZ));
-                
-                obj.transform.position = vec;
-                obj.transform.eulerAngles = angleVec;
-                
-            }
-        }
-
-        private void FixedUpdate()
-        {
-            Send();
+            UniTask.Run(() => { });
         }
 
         public void SetCurrentGameObject(GameObject playerGameObject)
