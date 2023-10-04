9bd2386007f704f8407bff4fa723aaccf0b34797
maesuke269 <maesuke1221@gmail.com>
2023-10-04 23:48:24 +0900

    create class SettingDisplayName for setting displayname

---
+++ Assets/scripts/Lobby/SettingDisplayName.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lobby
+{ 
+    /// <summary>
+    /// Class for setting display name
+    /// </summary>
+    public class SettingDisplayName : MonoBehaviour
+    {
+        
+    }
+}
