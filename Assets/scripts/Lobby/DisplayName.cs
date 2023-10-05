94314e0f6b703131ca019762c2acf2a4757b2522
maesuke269 <maesuke1221@gmail.com>
2023-10-05 14:01:28 +0900

    create change between json string

--- Assets/scripts/Lobby/DisplayName.cs
+++ Assets/scripts/Lobby/DisplayName.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRoidSDK.Examples.Core.View.Parts;
 
 namespace Lobby
 {
@@ -10,6 +12,29 @@ namespace Lobby
     public class DisplayName : MonoBehaviour
     {
         public readonly string UserId;
-        public readonly string DisplayName;
+        public readonly string NewDisplayName;
+        
+        public DisplayName(string userId, string newDisplayName)
+        {
+            UserId = userId;
+            NewDisplayName = newDisplayName;
+        }
+        
+        public Message FromJson(string json)
+        {
+            var message = JsonUtility.FromJson<Message>(json);
+            return message;
+        }
+
+        public string ToJson()
+        {
+            var json = JsonUtility.ToJson(this);
+            return json;
+        }
+        
+        public override string ToString()
+        {
+            return $"{UserId}, {NewDisplayName}";
+        }
     }
 }
