7de1f0fef5ce46ca9a181e7c5367e6d517df8fb0
maesuke269 <maesuke1221@gmail.com>
2023-10-05 15:31:41 +0900

    create SettingDisplayNameWindow for setting SettingDisplayName UI

---
+++ Assets/scripts/Lobby/SettingDisplayNameWindow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lobby
+{
+
+    public class SettingDisplayNameWindow : MonoBehaviour
+    {
+        
+        void Update()
+        {
+
+        }
+    }
+}
