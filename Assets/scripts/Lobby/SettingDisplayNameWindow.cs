e8d2898854522d5084dddd9e88a3721298a70580
maesuke269 <maesuke1221@gmail.com>
2023-10-09 01:57:18 +0900

    add windowsize variable

--- Assets/scripts/Lobby/SettingDisplayNameWindow.cs
+++ Assets/scripts/Lobby/SettingDisplayNameWindow.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 namespace Lobby
 {
-
+    /// <summary>
+    ///     Class for creating setting display name window
+    /// </summary>
     public class SettingDisplayNameWindow : MonoBehaviour
     {
         [SerializeField] private Texture2D enterNameTexture;
         [SerializeField] private Texture2D sendButtonTexture;
+        
+        private string _displayName = string.Empty;
+        
+        private Vector2 _screenSize = new(0, 0);
+        private Rect _windowRect = new(0, 0, 400, 400);
         void Update()
         {
 
