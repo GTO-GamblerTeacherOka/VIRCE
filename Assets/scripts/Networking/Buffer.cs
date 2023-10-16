06b3465f1f7035b3af59e1d4d9389269cb153fd5
snct-ukai <r02i08@ed.cc.suzuka-ct.ac.jp>
2023-10-16 22:36:50 +0900

    update: data send timing

--- Assets/scripts/Networking/Buffer.cs
+++ Assets/scripts/Networking/Buffer.cs
@@ -1,15 +1,17 @@
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Networking
 {
     public class Buffer
     {
         private static Buffer _instance;
-        private readonly Dictionary<byte, byte[]> _dictionary;
+        private readonly Dictionary<byte, Vector3[]> _dictionary;
 
         private Buffer()
         {
-            _dictionary = new Dictionary<byte, byte[]>();
+            _dictionary = new Dictionary<byte, Vector3[]>();
         }
 
         public static Buffer Instance
@@ -19,15 +21,31 @@ namespace Networking
 
         public void InputBuf(byte userId, byte[] data)
         {
-            _dictionary[userId] = data;
+            var x = data[..4];
+            var y = data[4..8];
+            var z = data[8..12];
+            var vec = new Vector3(
+                BitConverter.ToSingle(x),
+                BitConverter.ToSingle(y),
+                BitConverter.ToSingle(z));
+
+            var angleX = data[12..16];
+            var angleY = data[16..20];
+            var angleZ = data[20..24];
+
+            var angleVec = new Vector3(
+                BitConverter.ToSingle(angleX),
+                BitConverter.ToSingle(angleY),
+                BitConverter.ToSingle(angleZ));
+            _dictionary[userId] = new[] { vec, angleVec };
         }
 
-        public Dictionary<byte, byte[]> GetBuf()
+        public Dictionary<byte, Vector3[]> GetBuf()
         {
-            return new Dictionary<byte, byte[]>(_dictionary);
+            return new Dictionary<byte, Vector3[]>(_dictionary);
         }
 
-        public byte[] GetBuf(byte id)
+        public Vector3[] GetBuf(byte id)
         {
             return _dictionary[id];
         }
