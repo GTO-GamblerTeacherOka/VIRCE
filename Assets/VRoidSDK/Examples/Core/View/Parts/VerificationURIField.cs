80006ab9649dd7df093f9f7c55bf2a79774e5de8
Ukai Shota <73654679+snct-ukai@users.noreply.github.com>
2023-11-18 23:01:21 +0900

    remove: vroid sdk files

--- Assets/VRoidSDK/Examples/Core/View/Parts/VerificationURIField.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VRoidSDK.Examples.Core.View.Parts
-{
-    public class VerificationURIField : BaseView
-    {
-        public Message verificationURIDescription;
-        public Message verificationURI;
-    }
-}
